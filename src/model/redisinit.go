672ec791bdef19bc4fba9719e714c11a3e61dec0
Riter <767641495@qq.com>
2022-02-14 22:04:55 +0800

    添加了redis获取连接后的关闭操作

--- src/model/redisinit.go
+++ src/model/redisinit.go
@@ -6,19 +6,21 @@ import (
 
 func init() {
 	// 清空redis并将mysql中的表注入redis
-	get := database.RedisClient.Get()
-	get.Flush()
+	rc := database.RedisClient.Get()
+	defer rc.Close()
+
+	rc.Flush()
 
 	var courses []Course
 	db.Model(&Course{}).Find(&courses)
 	for _, v := range courses {
-		get.Do("HSET", "CourseToCount", v.CourseID, v.Capacity-v.CapSelected)
-		get.Do("HSET", "CourseToName", v.CourseID, v.Name)
+		rc.Do("HSET", "CourseToCount", v.CourseID, v.Capacity-v.CapSelected)
+		rc.Do("HSET", "CourseToName", v.CourseID, v.Name)
 	}
 
 	var binds []Bind
 	db.Model(&Bind{}).Find(&binds)
 	for _, v := range binds {
-		get.Do("HSET", "CourseToTeacher", v.CourseID, v.TeacherID)
+		rc.Do("HSET", "CourseToTeacher", v.CourseID, v.TeacherID)
 	}
 }
